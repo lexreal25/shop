8f9f2263fdec0613a21f2f914a9c0a6560177610
Daniel Nagai <daniel@shop-office.net>
2020-03-27 12:00:36 +0000

    sotrage.js

--- storage.js
+++ storage.js
@@ -21,7 +21,49 @@ if (CheckBrowser()) {
     }
     //if no item exist in the cart
     if (list == "<tr><th>Item</th><th>value</th></tr>\n") {
-        list += "<tr><td><i>empty</i></td><"
+        list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        //Bind the data to HTML table
+        document.getElementById('list').innerHTML = list;
+
         
+    }else {
+        alert('Cannot save shopping list as your browser does not support HTML5');
     }
+}
+localStorage.setItem('propertyName', 'value');
+localStorage.getItem('propertyName');
+localStorage.removeItem('propertyName');
+
+function SaveItem() {
+    var name= document.forms.ShoppingList.name.value;
+    var data= document.forms.ShoppingList.data.value;
+    localStorage.setItem(name, data);
+    doShowAll();
+}
+//Change an existing key-value in HTML5 storage.
+
+function ModifyItem(){
+    var name1= document.forms.ShoppingList.name.value;
+    var data1 = document.forms.ShoppingList.data.value;
+
+    //check if name1 exists.
+    if(localStorage.getItem(name1) !=null)
+    {
+        //update
+        localStorage.setItem(name1,data1);
+        document.forms.ShoppingList.data.value = localStorage;
+
+    }
+    doShowAll();
+}
+
+function RemoveItem() {
+    var name=document.forms.ShoppingList.name.value;
+    document.forms.ShoppingList.data.value = localStorage.removeItem(name);
+    doShowAll();
+}
+
+function ClearAll(){
+    localStorage.clear();
+    doShowAll();
 }
